961a232ed95e47c9d4b015a568c3ccbc6eb742ef
John Brooks <john@fastquake.com>
2014-01-09 17:38:39 -0500

    make it not look like "this"

--- fragment.glsl
+++ fragment.glsl
@@ -10,5 +10,5 @@ in float depth;
 void main(){
 	texColour = texture2D(texture,f_texcoord);
 	outColour = texColour*vec4(lightD,lightD,lightD,1.0);
-	outColour = vec4(1.0)*depth*(texColour*0+1);
+//	outColour = vec4(1.0)*depth*(texColour*0+1);
 }
