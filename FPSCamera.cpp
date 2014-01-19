28360d0e1ab81173b3297b779ccd3cc85797b344
John Brooks <john@fastquake.com>
2014-01-18 21:42:09 -0500

    Fix crashing and strafe speed

--- FPSCamera.cpp
+++ FPSCamera.cpp
@@ -13,9 +13,9 @@ float toRad(float degrees){
 
 FPSCamera::FPSCamera(float x, float y, float z){
 	pos = glm::vec3(x,y,z);
-	right = glm::vec3(1,0,0);
-	lookat = glm::vec3(0,0,-1);
-	up = glm::vec3(0,1,0);
+	right = glm::vec3(1.0,0.0,0.0);
+	lookat = glm::vec3(0.0,0.0,-1.0);
+	up = glm::vec3(0.0,1.0,0.0);
 }
 
 void FPSCamera::move(float amount){
@@ -23,14 +23,23 @@ void FPSCamera::move(float amount){
 }
 
 void FPSCamera::strafe(float amount){
-	pos += amount*right;
+	pos += amount*glm::normalize(right);
 }
 
 void FPSCamera::turn(glm::vec2 amount){
+	//FIXME: Camera flips when moved all the way up or down
 	right = glm::cross(lookat,up);
-	glm::mat4 turn = glm::rotate(glm::mat4(1),-amount.x,up) *
-					 glm::rotate(glm::mat4(1),-amount.y,right);
-	lookat = glm::mat3(turn)*lookat;
+	glm::mat4 turn = glm::rotate(glm::mat4(1.0),-amount.x,up) *
+					 glm::rotate(glm::mat4(1.0),-amount.y,right);
+
+	//Angle between new lookat and up; if this is less than 2 or greater than 358, then do not apply the change
+	float vecAngle = acos(glm::dot(glm::mat3(turn)*lookat,up));
+	if(vecAngle != vecAngle || vecAngle <= PI/90.0 || vecAngle >= PI-PI/90.0){
+			lookat = glm::mat3(glm::rotate(glm::mat4(1.0),-amount.x,up))*lookat; //Do apply the yaw change, however
+		}else{
+			lookat = glm::mat3(turn)*lookat;
+			cout << lookat.x << "," << lookat.y << "," << lookat.z << endl;
+		}
 }
 
 
