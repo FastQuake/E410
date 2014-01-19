0e73211d6f1072b3ee10397b3567c58cf3175951
John Brooks <john@fastquake.com>
2014-01-18 22:39:59 -0500

    Use quaternions for camera rotation

--- FPSCamera.cpp
+++ FPSCamera.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cfloat>
 #include "FPSCamera.hpp"
 #include "globals.hpp"
 using namespace std;
@@ -29,15 +30,19 @@ void FPSCamera::strafe(float amount){
 void FPSCamera::turn(glm::vec2 amount){
 	//FIXME: Camera flips when moved all the way up or down
 	right = glm::cross(lookat,up);
-	glm::mat4 turn = glm::rotate(glm::mat4(1.0),-amount.x,up) *
-					 glm::rotate(glm::mat4(1.0),-amount.y,right);
+	glm::vec3 oldLookat;
+	glm::quat pitch = glm::angleAxis(-amount.y,glm::normalize(right));
+	glm::quat yaw = glm::angleAxis(-amount.x,glm::normalize(up));
+	glm::quat rotator = pitch*yaw;
+	/*glm::mat4 turn = glm::rotate(glm::mat4(1.0),-amount.x,up) *
+					 glm::rotate(glm::mat4(1.0),-amount.y,right);*/
 
-	//Angle between new lookat and up; if this is less than 2 or greater than 358, then do not apply the change
-	float vecAngle = acos(glm::dot(glm::mat3(turn)*lookat,up));
+	//Angle between new lookat and up; if this is less than 2 or greater than 358, then do not apply the change. This prevents crashing.
+	float vecAngle = acos(glm::dot(rotator*lookat,up));
 	if(vecAngle != vecAngle || vecAngle <= PI/90.0 || vecAngle >= PI-PI/90.0){
-			lookat = glm::mat3(glm::rotate(glm::mat4(1.0),-amount.x,up))*lookat; //Do apply the yaw change, however
+			lookat = yaw*lookat; //Do apply the yaw change, however
 		}else{
-			lookat = glm::mat3(turn)*lookat;
+			lookat = rotator*lookat;
 			cout << lookat.x << "," << lookat.y << "," << lookat.z << endl;
 		}
 }
--- FPSCamera.hpp
+++ FPSCamera.hpp
@@ -3,6 +3,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 class FPSCamera{
 	private:
