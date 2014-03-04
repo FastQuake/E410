4724df39ea68a085d60d31c71d8733687a11dbf5
John Brooks <john@fastquake.com>
2014-03-03 19:44:16 -0500

    Update main.lua with test scene, fix rendering rotation

--- data/scripts/main.lua
+++ data/scripts/main.lua
@@ -1,5 +1,11 @@
 function init()
 	fixit = GO.createIQM("mr_fixit.iqm")
+	floor = GO.createIQM("cube.iqm")
+	shit = GO.createIQM("monkey.iqm")
+
+	GO.setScale(floor,10,0.1,10)
+	GO.setPos(floor,0,-0.5,0)
+	GO.setPos(shit,5,1.1,0)
 end
 
 function update(dt)
