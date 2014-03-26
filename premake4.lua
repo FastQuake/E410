cdb54406e19b5dcf5483d0ab60e89eb42da63252
Lucas Fryzek <nercos7@gmail.com>
2014-03-26 01:13:49 -0400

    statically link some stuff

--- premake4.lua
+++ premake4.lua
@@ -4,14 +4,14 @@ solution("E410")
 		project("E410")
 			kind("COnsoleApp")
 			language("C++")
-			files({"**.hpp","**.cpp"})
+			files({"**.hpp","**.cpp","liblua.a"})
 			libdirs({os.findlib("gl","glew","lua5.2")})
 			if os.is("windows") == true then
 				links({"sfml-system","sfml-window","sfml-graphics","glew32","opengl32","lua52"})
 			elseif os.is("linux") then
 				buildoptions({"`pkg-config --cflags lua5.2`"})
-				linkoptions({"-Bstatic `pkg-config --libs lua5.2` -Bdynamic"})
-				links({"sfml-system","sfml-window","sfml-graphics","GLEW","GL"})
+				linkoptions({"../liblua.a","../libGLEW.a"})
+				links({"sfml-system","sfml-window","sfml-graphics","GL",})
 			end
 			location("./build")
 
