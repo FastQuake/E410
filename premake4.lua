d985c2b39309ba804282d097f638125441ccec13
Lucas Fryzek <nercos7@gmail.com>
2014-03-25 23:49:33 -0400

    improve premake file

--- premake4.lua
+++ premake4.lua
@@ -5,11 +5,13 @@ solution("E410")
 			kind("COnsoleApp")
 			language("C++")
 			files({"**.hpp","**.cpp"})
-			libdirs({os.findlib("gl","glew","lua")})
+			libdirs({os.findlib("gl","glew","lua5.2")})
 			if os.is("windows") == true then
-				links({"sfml-system","sfml-window","sfml-graphics","glew32","opengl32","lua"})
-			else 
-				links({"sfml-system","sfml-window","sfml-graphics","GLEW","GL","lua"})
+				links({"sfml-system","sfml-window","sfml-graphics","glew32","opengl32","lua52"})
+			elseif os.is("linux") then
+				buildoptions({"`pkg-config --cflags lua5.2`"})
+				linkoptions({"`pkg-config --libs lua5.2`"})
+				links({"sfml-system","sfml-window","sfml-graphics","GLEW","GL"})
 			end
 			location("./build")
 
