53f4838d16996d0a0e4658d687d8829d01875844
Lucas Fryzek <nercos7@gmail.com>
2014-03-26 18:31:19 -0400

    adjust project file again

--- premake4.lua
+++ premake4.lua
@@ -11,7 +11,7 @@ solution("E410")
 			elseif os.is("linux") then
 				buildoptions({"`pkg-config --cflags lua5.2`"})
 				linkoptions({"`pkg-config --libs lua5.2`","-Wl,-rpath=./libs "})
-				links({"sfml-system","sfml-window","sfml-graphics","GL","GLEW","lua"})
+				links({"sfml-system","sfml-window","sfml-graphics","GL","GLEW"})
 			end
 			location("./build")
 
