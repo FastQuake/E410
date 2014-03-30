9bbe8dc39a4957db8123ca565cb1c3984224888a
John Brooks <john@fastquake.com>
2014-03-30 15:16:46 -0400

    Add Light class files

---
+++ Graphics/Light.cpp
@@ -0,0 +1,11 @@
+#include "Light.hpp"
+
+glm::mat4 Light::mvp(){
+glm::mat4 model = glm::translate(glm::mat4(1.0f),-pos);
+glm::mat4 view =	glm::rotate(glm::mat4(1.0f),rot.x,glm::vec3(1.0,0.0,0.0))*
+glm::rotate(glm::mat4(1.0f),rot.y,glm::vec3(0.0,1.0,0.0))*
+glm::rotate(glm::mat4(1.0f),rot.z,glm::vec3(0.0,0.0,1.0));
+glm::mat4 projection = glm::ortho<float>(-10.0f,10.0f,-10.0f,10.0f,-10.0f,50.0f);
+
+return projection*view*model;
+}
