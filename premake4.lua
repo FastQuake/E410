4a8f21973320a0775d9a938ad93a45fd54114420
Lucas Fryzek <nercos7@gmail.com>
2014-04-01 23:58:49 -0400

    fix enet on linking on windows

--- premake4.lua
+++ premake4.lua
@@ -7,7 +7,7 @@ solution("E410")
 			files({"**.hpp","**.cpp"})
 			libdirs({os.findlib("gl","glew","lua5.2")})
 			if os.is("windows") == true then
-				links({"sfml-system","sfml-window","sfml-graphics","glew32","opengl32","lua52","enet"})
+				links({"sfml-system","sfml-window","sfml-graphics","glew32","opengl32","lua52","enet","ws2_32","wsock32","winmm"})
 			elseif os.is("linux") then
 				buildoptions({"`pkg-config --cflags lua5.2`"})
 				linkoptions({"`pkg-config --libs lua5.2`","-Wl,-rpath=./libs "})
