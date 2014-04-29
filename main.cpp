f9c49cb03e2173b81b6a41920ac16b5dd03a088c
Lucas Fryzek <nercos7@gmail.com>
2014-04-29 17:21:58 -0400

    enable GL_TEXTURE_CUBE_MAP when drawing

--- main.cpp
+++ main.cpp
@@ -400,6 +400,7 @@ int main(int argc, char *argv[]){
 		glm::mat4 projection = glm::perspective(45.0f, 1.0f*width/height, 0.1f, 1000.0f);
 
 		//Do all drawing here
+		glEnable(GL_TEXTURE_CUBE_MAP);
 		glUseProgram(depthPrg.getID());
 		glBindFramebuffer(GL_FRAMEBUFFER,rendman.framebuffer);
 		for(int i=0;i<rendman.lights.size();i++)
@@ -410,6 +411,7 @@ int main(int argc, char *argv[]){
 		glUniformMatrix4fv(prg.getUniform("projection"),1,GL_FALSE,glm::value_ptr(projection));
 		glUniformMatrix4fv(prg.getUniform("pointProj"),1,GL_FALSE,glm::value_ptr(PLight::pointProjection));
 		rendman.render(&prg,dt.asSeconds());
+		glDisable(GL_TEXTURE_CUBE_MAP);
 
 		//Do sfml drawing here
 		gui->draw(&window);
