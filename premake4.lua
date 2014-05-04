8cb78830fc82e3d103e20830be743331a1c3d552
John Brooks <john@fastquake.com>
2014-05-03 21:08:35 -0400

    Add windows include dirs to premake (for build server)

--- premake4.lua
+++ premake4.lua
@@ -8,6 +8,7 @@ solution("E410")
 			libdirs({os.findlib("gl","glew","lua5.2")})
 			includedirs({"/usr/include/bullet/","/usr/local/include/bullet/"})
 			if os.is("windows") == true then
+				includedirs({"x86_64-w64-mingw32/include/","/usr/i686-w64-mingw32/include/"})
 				links({"sfml-system","sfml-window","sfml-graphics","glew32","opengl32","lua52","enet","ws2_32","wsock32","winmm","BulletCollision","BulletDynamics","LinearMath"})
 			elseif os.is("linux") then
 				buildoptions({"`pkg-config --cflags lua5.2`"})
